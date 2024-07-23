497111edf87eecafa2a05d7548d662e82db936d1
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-07-23 12:12:32 -0300

    init: :tada: Commit Initial!

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const person = {
+  mario: {
+    velocidade: 4,
+    manobra: 3,
+    poder: 3,
+    ponto: 0
+  },
+  peach: {
+    velocidade: 4,
+    manobra: 4,
+    poder: 2,
+    ponto: 0
+  },
+  yoshi: {
+    velocidade: 3,
+    manobra: 4,
+    poder: 3,
+    ponto: 0
+  },
+  bowser: {
+    velocidade: 5,
+    manobra: 2,
+    poder: 4,
+    ponto: 0
+  },
+  luigi: {
+    velocidade: 2,
+    manobra: 4,
+    poder: 4,
+    ponto: 0
+  },
+  kong: {
+    velocidade: 3,
+    manobra: 2,
+    poder: 5,
+    ponto: 0
+  },
+}
+
+console.log(person.kong.poder)
