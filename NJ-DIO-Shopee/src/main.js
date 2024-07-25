5f79c3ace4c043f6b3a5dedbf36ec9b037ff7ee0
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-07-25 09:55:51 -0300

    feat: :sparkles: Adicionado funcionalidade de remoção de item do carrinho por ID

--- NJ-DIO-Shopee/src/main.js
+++ NJ-DIO-Shopee/src/main.js
@@ -1,12 +1,39 @@
 // Array contendo todos os itens no carrinho
 let myCart = [];
-
 // Importando função do carrinho de criar produto, calcular, deletar e remover.
-import { createProduct, calculateTotal, deleteItem, removeItem } from './services/cart.js'
+import * as cartService from './services/cart.js';
 
-let addItemCart1 = await createProduct(myCart, 'Sabão', 12.99, 4);
-let addItemCart2 = await createProduct(myCart, 'Detergente', 8.9, 2);
+console.log("Welcome to Shopee Store! \n\n")
 
-console.log("Welcome to Shopee Store! \n\n" + 'Meu carrinho: ' + JSON.stringify(myCart) + '\n')
-console.log(`Atualmente o carrinho tem R$ ${await calculateTotal(myCart)} em produtos! `)
+// Adição de itens no carrinho!
+let addItemCart1 = await cartService.createProduct(myCart, 'Sabão', 12.99, 4);
+let addItemCart2 = await cartService.createProduct(myCart, 'Detergente', 8.9, 2);
+console.log("Atualmente temos esse itens no carrinho! " + '\n\n'  + JSON.stringify(myCart) + '\n')
 
+// Função para calcular todos os itens do carrinho!
+async function valorTotal(){
+  let valorTotal = await cartService.calculateTotal(myCart)
+  console.log(`Atualmente o carrinho tem um total de R$ ${valorTotal} em produtos!\n`)
+}valorTotal();
+
+// Função para deletar item com valor mais alto!
+async function removeItem(){
+  console.log('1', myCart)
+  await cartService.removeItemHigherValue(myCart);
+  console.log(`Após a remoção seu carrinho ficou com esses itens: ${JSON.stringify(myCart)}\n`)
+}removeItem()
+
+
+// Função para deletar item pelo ID!
+async function removeItemID(){
+  let selectID = 1; // Seletor de ID de forma manual.
+  myCart = await cartService.removeItemId(myCart, selectID);
+  console.log(`Item ${selectID} deletado agora seu carrinho tem esses itens: ${JSON.stringify(myCart)}\n`)
+}removeItemID()
+
+
+// Função para deletar todos os itens do carrinho!
+async function deleteAll(){
+  myCart = await cartService.deleteItem(myCart);  
+  console.log(`Agora meu carrinho tem esses itens: ${myCart}\n`)
+}deleteAll()
--- NJ-DIO-Shopee/src/services/cart.js
+++ NJ-DIO-Shopee/src/services/cart.js
@@ -1,19 +1,42 @@
-// Cria o produto e adiciona no carrinho!
+let id = 0;
+// Função para criar o produto e adiciona no carrinho!
 export function createProduct(myCart, name, price, quantity) {
+  id++;
   const product = {
     item: name,
     price: price,
     quantity: quantity,
+    id: id,
     subtotal: () => price * quantity
   };
   myCart.push(product);
   return myCart;
 }
+// Função para calcular todos os itens do carrinho!
 export async function calculateTotal(myCart){
-  return myCart.reduce((total, item) => total + item.subtotal(), 0)
+  return myCart.reduce((total, item) => total + item.subtotal(), 0);
 }
-
-export async function deleteItem(myCart, name){}
-
-export async function removeItem(myCart, index){}
-
+// Função para remover o item mais caro do carrinho!
+export async function removeItemHigherValue(myCart){
+  let mostValue = await myCart.reduce((maxItem, currentItem) => {
+    if(currentItem.price > maxItem.price){
+      return currentItem;
+    }else{
+      return maxItem;
+    }
+  })
+  console.log('xxx', mostValue)
+  // Filtra o carrinho para remover o item com o maior valor
+  myCart = myCart.filter(item => item !== mostValue);
+}
+// Função para remover o item pelo ID!
+export async function removeItemId(myCart, selectID){
+  myCart = myCart.filter(obj => obj.id != selectID);
+  return myCart;
+}
+// Função para deletar todos os itens do carrinho!
+export async function deleteItem(myCart){
+  myCart.length = 0;
+  myCart.push('Carrinho Zerado!');
+  return myCart;
+}
